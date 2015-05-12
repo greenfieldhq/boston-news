0485f92143dd61c27e7c7d3da596b1aa59df1300
Mike Munroe <mike@greenfieldhq.com>
2015-05-12 16:58:44 -0400

    reformat and remove if

--- backend/app/controllers/api/search_controller.rb
+++ backend/app/controllers/api/search_controller.rb
@@ -1,7 +1,11 @@
 class Api::SearchController < ApplicationController
   def index
-    search = BostonNewsIndex.query(query_string: {fields: [:title, :body],
-      query: params[:query], default_operator: 'and'}) if params
+    search = BostonNewsIndex.query(query_string: {
+      fields: [:title, :body],
+      query: params[:query],
+      default_operator: 'and'
+    })
+
     news = search.load()
 
     render json: news
